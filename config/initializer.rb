2772bca77b0e111b77a74a6e15565d352ed98f3c
Kremenchuk <kremenchuk@bk.ru>
2016-10-04 21:51:14 +0300

    1

---
+++ config/initializer.rb
@@ -0,0 +1,6 @@
+if $exchanger_modules_to_sidebar.present?
+  $exchanger_modules_to_sidebar << :my_first_gem
+else
+  $exchanger_modules_to_sidebar = []
+  $exchanger_modules_to_sidebar << :my_first_gem
+end
