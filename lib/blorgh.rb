61b1ee06adeb2700cabf4ba12f57a597930513fb
rem <kremartem@gmail.com>
2016-10-05 17:38:43 +0300

    1

--- lib/blorgh.rb
+++ lib/blorgh.rb
@@ -2,11 +2,28 @@ require "blorgh/engine"
 
 module Blorgh
   if $exchanger_modules_to_sidebar.present?
-    Blorgh.add_to_const_blorgh
+    module_function :add_to_const_blorgh
   else
     $exchanger_modules_to_sidebar = []
-    Blorgh.add_to_const_blorgh
+    module_function :add_to_const_blorgh
   end
+
+  # def self.method_one
+  # end
+  #
+  # def Foo.method_two
+  # end
+  #
+  # class << self
+  #   def method_three
+  #   end
+  # end
+  #
+  # def method_four
+  # end
+  #
+  # module_function :method_four
+
 
   def add_to_const_blorgh
     $exchanger_modules_to_sidebar << {[:gem] => :my_second_gem}
