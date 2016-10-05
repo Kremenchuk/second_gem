0990db4a26559594eb18b21dde9315345c5c3fce
rem <kremartem@gmail.com>
2016-10-05 17:22:07 +0300

    1

--- lib/blorgh.rb
+++ lib/blorgh.rb
@@ -2,9 +2,9 @@ require "blorgh/engine"
 
 module Blorgh
   if $exchanger_modules_to_sidebar.present?
-    $exchanger_modules_to_sidebar << :my_first_gem
+    $exchanger_modules_to_sidebar << :my_second_gem
   else
     $exchanger_modules_to_sidebar = []
-    $exchanger_modules_to_sidebar << :my_first_gem
+    $exchanger_modules_to_sidebar << :my_second_gem
   end
 end
