cea8efa2e57ec5b87c2b34d5cb818aa4d886f86f
rem <kremartem@gmail.com>
2016-10-05 17:51:35 +0300

    1

---
+++ lib/blorgh/initializer.rb
@@ -0,0 +1,36 @@
+module Blorgh
+  class Blorgh
+  if $exchanger_modules_to_sidebar.present?
+    # module_function :add_to_const_blorgh
+    method_one
+  else
+    $exchanger_modules_to_sidebar = []
+    method_one
+    # module_function :add_to_const_blorgh
+  end
+
+  def self.method_one
+    $exchanger_modules_to_sidebar << {:gem => :my_second_gem}
+    $exchanger_modules_to_sidebar << {:title => 'My second_gem'}
+  end
+  #
+  # def Blorgh.method_two
+  # end
+  #
+  # class << self
+  #   def method_three
+  #   end
+  # end
+  #
+  # def method_four
+  # end
+
+
+
+
+  # def add_to_const_blorgh
+  #   $exchanger_modules_to_sidebar << {[:gem] => :my_second_gem}
+  #   $exchanger_modules_to_sidebar << {[:title] => 'My second_gem'}
+  # end
+  end
+end
