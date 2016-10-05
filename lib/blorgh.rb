f67820c342803ba2a3754cb1cbb020c44a88874b
rem <kremartem@gmail.com>
2016-10-05 17:30:18 +0300

    1

--- lib/blorgh.rb
+++ lib/blorgh.rb
@@ -2,13 +2,13 @@ require "blorgh/engine"
 
 module Blorgh
   if $exchanger_modules_to_sidebar.present?
-    add_to_const
+    add_to_const_blorgh
   else
     $exchanger_modules_to_sidebar = []
-    add_to_const
+    add_to_const_blorgh
   end
 
-  def self.add_to_const
+  def self.add_to_const_blorgh
     $exchanger_modules_to_sidebar << {[:gem] => :my_second_gem}
     $exchanger_modules_to_sidebar << {[:title] => 'My second_gem'}
   end
