58fd0d954e0886fd1a86c13696accd38411f8f82
rem <kremartem@gmail.com>
2016-10-05 17:40:02 +0300

    1

---
+++ config/initializer.rb
@@ -0,0 +1,4 @@
+def add_to_const_blorgh
+  $exchanger_modules_to_sidebar << {[:gem] => :my_second_gem}
+  $exchanger_modules_to_sidebar << {[:title] => 'My second_gem'}
+end
--- lib/blorgh.rb
+++ lib/blorgh.rb
@@ -25,8 +25,5 @@ module Blorgh
   # module_function :method_four
 
 
-  def add_to_const_blorgh
-    $exchanger_modules_to_sidebar << {[:gem] => :my_second_gem}
-    $exchanger_modules_to_sidebar << {[:title] => 'My second_gem'}
-  end
+
 end
