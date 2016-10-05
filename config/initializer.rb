6b52c443c374d613b7edac459773c6407d5d0289
rem <kremartem@gmail.com>
2016-10-05 17:41:34 +0300

    1

--- config/initializer.rb
+++
@@ -1,4 +0,0 @@
-def add_to_const_blorgh
-  $exchanger_modules_to_sidebar << {[:gem] => :my_second_gem}
-  $exchanger_modules_to_sidebar << {[:title] => 'My second_gem'}
-end
--- lib/blorgh.rb
+++ lib/blorgh.rb
@@ -2,28 +2,32 @@ require "blorgh/engine"
 
 module Blorgh
   if $exchanger_modules_to_sidebar.present?
-    module_function :add_to_const_blorgh
+    # module_function :add_to_const_blorgh
+    method_one
   else
     $exchanger_modules_to_sidebar = []
-    module_function :add_to_const_blorgh
+    # module_function :add_to_const_blorgh
   end
 
-  # def self.method_one
-  # end
-  #
-  # def Foo.method_two
-  # end
-  #
-  # class << self
-  #   def method_three
-  #   end
-  # end
-  #
-  # def method_four
-  # end
-  #
-  # module_function :method_four
+  def self.method_one
+  end
+
+  def Blorgh.method_two
+  end
+
+  class << self
+    def method_three
+    end
+  end
+
+  def method_four
+  end
 
 
 
+
+  # def add_to_const_blorgh
+  #   $exchanger_modules_to_sidebar << {[:gem] => :my_second_gem}
+  #   $exchanger_modules_to_sidebar << {[:title] => 'My second_gem'}
+  # end
 end
