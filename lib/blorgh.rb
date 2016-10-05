2a757172635c063647b053b478d951b7b3f72890
rem <kremartem@gmail.com>
2016-10-05 17:35:55 +0300

    1

--- lib/blorgh.rb
+++ lib/blorgh.rb
@@ -8,7 +8,7 @@ module Blorgh
     Blorgh.add_to_const_blorgh
   end
 
-  def self.add_to_const_blorgh
+  def add_to_const_blorgh
     $exchanger_modules_to_sidebar << {[:gem] => :my_second_gem}
     $exchanger_modules_to_sidebar << {[:title] => 'My second_gem'}
   end
