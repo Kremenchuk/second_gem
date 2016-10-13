e790a155339796e8898c50da6aca320abd27559c
rem <kremartem@gmail.com>
2016-10-13 18:38:05 +0300

    1

--- app/controllers/blorgh/my_controller.rb
+++ app/controllers/blorgh/my_controller.rb
@@ -1,8 +1,8 @@
 module Blorgh
   class MyController < ApplicationController
     def index
-      User.create(name: 'Asd', email: 'qwe')
-      @users = User.all
+      # User.create(name: 'Asd', email: 'qwe')
+      # @users = User.all
     end
   end
 end
