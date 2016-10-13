9d57ddfbb822fed170e14cd37140c006f4e9faec
rem <kremartem@gmail.com>
2016-10-13 18:23:58 +0300

    1

--- lib/blorgh.rb
+++ lib/blorgh.rb
@@ -13,12 +13,5 @@ module Blorgh
     add_to_const_blorgh
   end
 
-  initializer :append_migrations do |app|
-    unless app.root.to_s.match root.to_s
-      config.paths["db/migrate"].expanded.each do |expanded_path|
-        app.config.paths["db/migrate"] << expanded_path
-      end
-    end
-  end
 
 end
