52f61f0d2026983b91e5e84826f92cfda305a1be
rem <kremartem@gmail.com>
2016-10-14 16:27:26 +0300

    1

--- config/initializers/blorgh.rb
+++ config/initializers/blorgh.rb
@@ -1,1 +1,10 @@
-Rails.configuration.x.require_gem += {:gem => 'my_second_gem', :title => 'My second gem'}
+def self.add_to_const_blorgh
+  Rails.configuration.x.require_gem << {:gem => 'my_second_gem', :title => 'My second gem'}
+end
+
+if Rails.configuration.x.require_gem.present?
+  add_to_const_blorgh
+else
+  Rails.configuration.x.require_gem = []
+  add_to_const_blorgh
+end
--- lib/blorgh.rb
+++ lib/blorgh.rb
@@ -2,16 +2,5 @@ require "blorgh/engine"
 require "blorgh/initializer"
 
 module Blorgh
-  def self.add_to_const_blorgh
-    $exchanger_modules_to_sidebar << {:gem => 'my_second_gem', :title => 'My second gem'}
-  end
-
-  if $exchanger_modules_to_sidebar.present?
-    add_to_const_blorgh
-  else
-    $exchanger_modules_to_sidebar = []
-    add_to_const_blorgh
-  end
-
 
 end
