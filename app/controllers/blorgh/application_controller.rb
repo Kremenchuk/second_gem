26b46856c3631033053fc66e1a4c03862035e9ab
rem <kremartem@gmail.com>
2016-10-14 16:12:46 +0300

    1

--- app/controllers/blorgh/application_controller.rb
+++ app/controllers/blorgh/application_controller.rb
@@ -2,5 +2,6 @@ module Blorgh
   class ApplicationController < ActionController::Base
     layout false
     protect_from_forgery with: :exception
+    Application.config.x.require_gem << {:gem => 'my_second_gem', :title => 'My second gem'}
   end
 end
