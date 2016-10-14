d9c559f6fe431a78c9cf3173bd4471fe7fe86322
rem <kremartem@gmail.com>
2016-10-14 17:57:25 +0300

    1

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,4 @@
 Rails.application.routes.draw do
   get 'my_second_gem' => 'blorgh/my#index'
 end
-Rails.application.config.x.menu_items << { :name => 'Product Attributes', :path => Rails.application.routes.url_helpers.product_attributes_path }
+Rails.application.config.x.menu_items << { :name => 'Product Attributes', :path => Rails.application.routes.url_helpers.my_second_gem_path }
